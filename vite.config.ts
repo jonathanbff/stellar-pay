b3ffd255413ae4bf5b1239c87f7290b51b56dd55
bellujrb <69083498+jonathanbff@users.noreply.github.com>
2025-08-09 21:45:46 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stellar-pay/',
+  base: '/', // Mudança aqui - remover '/stellar-pay/'
   css: {
     preprocessorOptions: {
       scss: {
